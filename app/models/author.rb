606bd2bf8bba7a4d96ade18cd47e74eb43545e67
ejgann <aspentree79@gmail.com>
2019-11-29 07:47:28 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,10 @@
 class Author < ActiveRecord::Base
+    validates :name, presence: true
+    validates :email, uniqueness: true
 end
+
+# The name cannot be blank
+# The e-mail is unique
+
+# Author.create(name: "John Doe").valid?
+# Author.create(name: nil).valid?
